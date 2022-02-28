e87a2c800e2e598a42877a54dbbd2f3d83d43474
Lee <lee@asgsolutions.co.za>
2022-02-28 21:36:02 +0200

    main * Create Calculator styling * Include sass, bootstrap and react-bootstrap * start html and framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "react-bootstrap/Button";
+import './styles/calculator/_calculator.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <div className="calculator-container mt-4">
+          <div className="display-calculations">
+              <div className="row">
+                  <div className="d-flex
+                  previous-entry
+                  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12
+                  justify-content-end"
+                  >
+                      50000
+                  </div>
+              </div>
+              <div className="row current-entry-row">
+                  <div className="d-flex
+                  current-entry-detail
+                  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12
+                  justify-content-end"
+                  >
+                      2000
+                  </div>
+              </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
